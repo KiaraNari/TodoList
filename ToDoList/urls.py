ecba984c26861044443de4fcb28d01eac255e6e9
Darina <shpak.darina@gmail.com>
2017-11-27 00:40:40 +0200

    Changes in urls.py

--- ToDoList/urls.py
+++ ToDoList/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 
 
 urlpatterns = [
-    # url(r'^$', include('todos.urls')),
+    url(r'^$', include('todos.urls')),
     url(r'^todos/', include('todos.urls')),
     url(r'^admin/', admin.site.urls),
 
